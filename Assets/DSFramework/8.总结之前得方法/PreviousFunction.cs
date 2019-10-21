0728bea4e07604e35e0ec32835beb0d161a117c0
Dimitri Wang <54432076+DimitriWang@users.noreply.github.com>
2019-10-21 14:51:34 +0800

    Integration of update API
    The old API was phased out

--- Assets/DSFramework/8.总结之前得方法/PreviousFunction.cs
+++ Assets/DSFramework/8.总结之前得方法/PreviousFunction.cs
@@ -17,102 +17,111 @@ using UnityEditor;
 
 namespace DSFramework
 {
-    
-    public class PreviousFunction : MonoBehaviour
+    public class CommonUtil
     {
-#if UNITY_EDITOR
-        /// <summary>
-        /// 获取文件名
-        /// </summary>
-        /// <returns></returns>
-        public static string GeneratePackageName()
-        {
-            return "DSFramework_" + DateTime.Now.ToString("yyyyMMdd_HH");
-
-        }
-
-        /// <summary>
-        /// 复制文本到剪切板
-        /// </summary>
-        /// <param name="text"></param>
+        //复制文本到剪切板
         public static void CopyText(string text)
         {
             GUIUtility.systemCopyBuffer = text;
         }
+    }
 
-        /// <summary>
-        /// 导出包
-        /// </summary>
-        /// <param name="assetPathName"></param>
-        /// <param name="fileName"></param>
-        public static void ExportPackage(string assetPathName, string fileName)
+    public class Exporter
+    {
+        //获取文件名
+        public static string GeneratePackageName()
         {
-            AssetDatabase.ExportPackage(assetPathName, fileName, ExportPackageOptions.Recurse);
+            return "DSFramework_" + DateTime.Now.ToString("yyyyMMdd_HH");
         }
+    }
 
-        /// <summary>
-        /// 打开所在文件夹
-        /// </summary>
-        /// <param name="folderPath"></param>
+    public class EditorUtil
+    {
+#if UNITY_EDITOR
+
+        //打开所在文件夹
         public static void OpenInFolder(string folderPath)
         {
             Application.OpenURL("file:///" + folderPath);
         }
 
-        /// <summary>
-        /// MenuItem复用
-        /// </summary>
-        /// <param name="itemName"></param>
+        //MenuItem复用
         public static void CallMenuItem(string itemName)
         {
             EditorApplication.ExecuteMenuItem(itemName);
             Application.OpenURL("file:///" + Path.Combine(Application.dataPath, "../"));
         }
 
+        //导出包
+        public static void ExportPackage(string assetPathName, string fileName)
+        {
+            AssetDatabase.ExportPackage(assetPathName, fileName, ExportPackageOptions.Recurse);
+        }
+#endif
+    }
+
+    public class PreviousFunction : MonoBehaviour
+    {
+        [Obsolete("方法以过时,请使用 CommonUtil.CopyText(text);")]
+        public static void CopyText(string text)
+        {
+            CommonUtil.CopyText(text);
+        }
+        [Obsolete("方法以过时,请使用 Exporter.GeneratePackageName();")]
+        public static string GeneratePackageName()
+        {
+            
+            return Exporter.GeneratePackageName();
+        }
+        [Obsolete("方法以过时,请使用 EditorUtil.OpenInFolder(folderPath);")]
+        public static void OpenInFolder(string folderPath)
+        {
+            EditorUtil.OpenInFolder(folderPath);
+        }
+#if UNITY_EDITOR
+        [Obsolete("方法以过时,请使用 EditorUtil.CallMenuItem(itemName);")]
+        public static void CallMenuItem(string itemName)
+        {
+            EditorUtil.CallMenuItem(itemName);
+        }
+        [Obsolete("方法以过时,请使用 EditorUtil.ExportPackage(assetPathName,fileName);")]
+        public static void ExportPackage(string assetPathName, string fileName)
+        {
+            EditorUtil.ExportPackage(assetPathName, fileName);
+        }
+#endif
+#if UNITY_EDITOR
         [MenuItem("DSFramework/8.总结之前的方法/1.生成 UnityPackage 名字")]
         private static void MenuClicked()
         {
-            Debug.Log(GeneratePackageName());
+            Debug.Log(Exporter.GeneratePackageName());
         }
-
         [MenuItem("DSFramework/8.总结之前的方法/2.复制文本到剪切板")]
         private static void MenuClicked2()
         {
-            CopyText("要复制的文本");
+            CommonUtil.CopyText("要复制的文本");
         }
-
         [MenuItem("DSFramework/8.总结之前的方法/3.生成文件名到剪切板")]
         private static void MenuClicked3()
         {
-            CopyText(GeneratePackageName());
+            CommonUtil.CopyText(Exporter.GeneratePackageName());
         }
-
         [MenuItem("DSFramework/8.总结之前的方法/4.导出 UnityPackage")]
         private static void MenuClicked4()
         {
-            ExportPackage("Assets/DSFramework", GeneratePackageName() + ".unitypackage");
+            EditorUtil.ExportPackage("Assets/DSFramework", Exporter.GeneratePackageName() + ".unitypackage");
         }
-
         [MenuItem("DSFramework/8.总结之前的方法/5.打开所在文件夹")]
         private static void MenuClicked5()
         {
-            OpenInFolder(Application.dataPath);
+            EditorUtil.OpenInFolder(Application.dataPath);
         }
-
         [MenuItem("DSFramework/8.总结之前的方法/6.MenuItem复用")]
         private static void MenuClicked6()
         {
-            ExportPackage("Assets/DSFramework", GeneratePackageName() + ".unitypackage");
-            OpenInFolder(Path.Combine(Application.dataPath, "../"));
+            EditorUtil.ExportPackage("Assets/DSFramework", Exporter.GeneratePackageName() + ".unitypackage");
+            EditorUtil.OpenInFolder(Path.Combine(Application.dataPath, "../"));
         }
-
-        [MenuItem("DSFramework/8.总结之前的方法/7.自定义快捷键")]
-        private static void MenuClicked7()
-        {
-            Debug.Log("%e ctrl/cmd + e");
-            //EditorApplication.ExecuteMenuItem("DSFramework/8.总结之前的方法/6.MenuItem复用");
-        }
-
 #endif
     }
 }
